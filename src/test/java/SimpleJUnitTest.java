1eb85daea175b2b9605f95b6ab4f9e2d360feb37
Bulaev Artem <fobozzz@yandex.ru>
2024-12-04 18:21:24 +0300

    add beforeAll, beforeEach, afterEach, afterAll (the life cycle of the test)

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.*;
+
+
+public class SimpleJUnitTest {
+
+    int result;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("\n###-----beforeAll()\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("###   beforeEach()");
+        result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###   afterEach()\n");
+        result = 0;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("###-----fterAll()\n");
+    }
+
+    @Test
+        void firstTest() {
+        System.out.println("       #### @Test firstTest()");
+        Assertions.assertTrue(result > 2);
+
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("      #### @Test secondTest()");
+        Assertions.assertTrue(result > 2);
+
+    }
+
+    private int getResult() {
+        return 3;
+    }
+
+}
