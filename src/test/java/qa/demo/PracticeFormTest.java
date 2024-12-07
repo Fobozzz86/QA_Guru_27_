21df5e80bff08722b08cf0f2cde886bfc1f5a96c
Bulaev Artem <fobozzz@yandex.ru>
2024-12-07 13:53:13 +0300

    add successFillPracticeFormTest in proceed

--- src/test/java/qa/demo/PracticeFormTest.java
+++ src/test/java/qa/demo/PracticeFormTest.java
@@ -1,7 +1,46 @@
 package qa.demo;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class PracticeFormTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void successFillPracticeFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Artem");
+        $("#lastName").setValue("Bulaev");
+        $("#userEmail").setValue("Fooolll@test.com");
+        $("[for=gender-radio-1]").click();
+        $("#userNumber").setValue("89649990000");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__day--010").click();
+        $("#subjectsInput").setValue("Maths").sendKeys(Keys.ENTER);
+        $("#subjectsInput").setValue("bio").sendKeys(Keys.ENTER);
+        $("#subjectsInput").setValue("ch").sendKeys(Keys.ENTER);
+        $("[for=hobbies-checkbox-2]").click();
+        $("[for=hobbies-checkbox-3]").click();
+        $("#uploadPicture").uploadFromClasspath("img/Test.jpg");
+        $("#currentAddress").setValue("Penza");
 
 
 
+
+        sleep(600_000);
+
+
+
+    }
 }
