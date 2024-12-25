d836518d8da7f5082c32cedc8146e557393dfaf4
Bulaev Artem <fobozzz@yandex.ru>
2024-12-25 18:38:23 +0300

    Refactoring code, split PracticeFormTest and PracticeFormWithCommentsTest

--- src/test/java/qa/demo/PracticeFormWithCommentsTest.java
+++ src/test/java/qa/demo/PracticeFormWithCommentsTest.java
@@ -3,12 +3,13 @@ package qa.demo;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeFormTest {
+public class PracticeFormWithCommentsTest {
     @BeforeAll
     static void beforeAll() {
         //Configuration.browserSize = "1920x1080";
@@ -20,6 +21,7 @@ public class PracticeFormTest {
     void successFillPracticeFormTest() {
         open("/automation-practice-form");
 
+        //    Тест может упасть из-за всплывающих баннеров (ошибка element click intercepted), можно добавить
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
@@ -27,22 +29,25 @@ public class PracticeFormTest {
         $("#firstName").setValue("Artem");
         $("#lastName").setValue("Bulaev");
         $("#userEmail").setValue("Fooolll@test.com");
-        $("#genterWrapper").$(byText("Male")).click();
+        $("#genterWrapper").$(byText("Male")).click(); // Такой локатор более читабельный
+ //      $("[for=gender-radio-1]").click();
         $("#userNumber").setValue("89649990000");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1990");
+//        $(".react-datepicker__day--010").click(); //если на форме выбора числа нет дублирующих чисел
+        // Код когда на форме выбора числа есть дублирующиеся числа --30--
         $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#subjectsInput").setValue("Maths").sendKeys(Keys.ENTER);
         $("#subjectsInput").setValue("bio").pressEnter();
         $("#subjectsInput").setValue("ch").pressEnter();
         $("[for=hobbies-checkbox-3]").click();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click(); // оптимальный код
         $("#uploadPicture").uploadFromClasspath("img/Test.jpg");
         $("#currentAddress").setValue("Penza");
         $("#submit").scrollIntoView(true);
         $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText("NCR")).click(); // setTimeout(function() { debugger; }, 5000);
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
@@ -51,7 +56,7 @@ public class PracticeFormTest {
         $(".table-hover").shouldHave(text("Artem Bulaev"), text("Fooolll@test.com"),
                 text("Male"), text("8964999000"));
         $("#closeLargeModal").scrollIntoView(true).click();
-           $(".practice-form-wrapper").shouldHave(text("Practice Form"));
+        $(".practice-form-wrapper").shouldHave(text("Practice Form"));
 
 //        sleep(600_000);
 
